39dd9b39c7b7836c75e0025f81cbb287f121ee17
Ian Ker Arado Cajes <icajes@yotpo.com>
2025-01-01 16:13:23 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ import UgcGallery from "./components/UgcGallery";
 
 function App() {
   //Instance ID
-  let InstanceId_Widget = "993796";
+  let InstanceId_Widget = "996106";
   let InstanceId_QA = "";
   let InstanceId_SEO = "";
   let InstanceId_Carousel = "";
-  let InstanceId_StarRating = "995192";
+  let InstanceId_StarRating = "";
   let InstanceId_PromotedProd = "";
   let InstanceId_UgcGallery = "";
   let InstanceId_ReviewsTab = "";
